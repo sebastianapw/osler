b0d5ee6aee8a54911411b791b8aad78c594018c3
ericsallinger <eric.sallinger303@gmail.com>
2020-11-02 17:18:39 -0700

    azure dns change

--- config/settings/local-demo.py
+++ config/settings/local-demo.py
@@ -1,5 +1,4 @@
 from .local import *
 from .umkc import *
-
-ALLOWED_HOSTS += ["40.77.16.205"]
+from .demo import *
 
--- config/settings/production-demo.py
+++ config/settings/production-demo.py
@@ -2,4 +2,4 @@ from .production import *
 from .umkc import *
 from .demo import *
 
-ALLOWED_HOSTS += ["40.77.16.205"]
+ALLOWED_HOSTS += ["oslerdemo.centralus.cloudapp.azure.com"]
